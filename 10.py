a423b57150b302ad5dc26036f75402b5e15912d8
Tim415 <64966124+Tim415@users.noreply.github.com>
2021-02-03 11:52:45 +0800

    Add files via upload

--- 10.py
+++ 10.py
@@ -7,18 +7,17 @@ guesstimes=0
 for i in range(chance):
     guesstimes=guesstimes+1
     guess=int(input('請猜數字'))
+    while (guess>big or guess<small):
+        print('你這小笨蛋，你猜數字的根本不在範圍內！')
+        guess=int(input('請猜數字'))
     if guess==number:
         print('恭喜你答對了，你花了',guesstimes,'次')
         break
-    elif (guess>big or guess<small):
-        print('你這小笨蛋，你猜的數字根本不在範圍內！')
-        chance=chance+1
-        guesstimes=guesstimes-1
     elif guess<number:
         print('太小了!你還剩',chance-guesstimes,'次')
+        if chance-guesstimes==0:
+            print('你猜的次數已經用完了，正確答案是',number)
     elif guess>number:
         print('太大了!你還剩',chance-guesstimes,'次')
-    else:
-        print('你這大笨蛋!你猜的根本不是數字')
-        guesstimes=guesstimes-1
-        chance=chance+1
+        if chance-guesstimes==0:
+            print('你猜的次數已經用完了，正確答案是',number)
